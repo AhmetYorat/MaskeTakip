caac289df43bd6fc1f1285b84b56d13139951649
Ahmet YORAT <ahmetyorat0@gmail.com>
2024-03-21 16:59:05 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    { 
+    
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService)// bu blok constructor dur bu class new yapıldıgında ilk burası çalışır.
+        {
+            _applicantService = applicantService;
+        }
+
+        public void GiveMask(Person person)
+        {
+            //PersonManager personManager = new PersonManager();
+            //bir iş sınıfı başka bir iş sınıfını kullanırken newliyorsa ileriye dönük değişiklik talebinde direnç gösterir.
+            //yani newlediğimiz classa bağımlı hale geliriz
+
+
+            if (_applicantService.CheckPerson(person))//== true koymasak bile default olarak true alındıgı için true kabul edilir.
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi."); 
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske VERİLEMEDİ.");
+            }
+        }
+
+    }
+}
---
+++ WorkAround/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using Entities.Concrete;
+
+Console.WriteLine("Merhaba");
+
+static void SelamVer(string isim="isimsiz")
+{
+    Console.WriteLine("Selam" + " " + isim);
+}
+
+    SelamVer("Ahnet");
+    SelamVer("İlker");
+    SelamVer();
+
+static int Topla(int sayi1, int sayi2)
+{
+    int sonuc=sayi1 + sayi2;
+    Console.WriteLine("Toplam: " + sonuc);
+    return sonuc;
+}
+
+Topla(3, 6);
+
+
+string[] sehirler1 = { "Adana", "Mersin", "Balıkesir" };
+
+foreach (string s in sehirler1)
+{
+    Console.WriteLine(s);
+}
+
+List<string> yeniSehirler = new List<string> {"Adana1", "Mersin1", "Balıkesir1" };// üstteki dizi oluşturmayla bu aynı
+yeniSehirler.Add("Bursa1");
+
+foreach (string s in yeniSehirler)
+{
+    Console.WriteLine(s);
+}
+Person person1 = new Person();
+person1.FirstName = "Ahmet";
+person1.LastName = "Yorat";
+person1.Id = 10382387316;
+person1.DateOfBirthYear = 2005;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
